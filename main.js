488f1b59672b84d136cb988bfe65924f9aa9d730
Christina Persson <c.i.l.persson@gmail.com>
2020-09-18 12:14:22 +0200

    Updated readme

---
+++ main.js
@@ -0,0 +1,11 @@
+let app = new Vue({
+  el: "#app",
+  data: {
+    product: "Socks",
+    image:
+      "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
+    description: "A pair of socks that can be put on your feet.",
+    link: "https://en.wikipedia.org/wiki/Sock",
+    inStock: true,
+  },
+});
